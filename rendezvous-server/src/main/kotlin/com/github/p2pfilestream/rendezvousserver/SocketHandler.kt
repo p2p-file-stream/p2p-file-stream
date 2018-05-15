db7b2b2cc4734f09a36c8261a3e6ede6f7aa19ae
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-15 13:05:27 +0200

    Add WebSocket server

---
+++ rendezvous-server/src/main/kotlin/com/github/p2pfilestream/rendezvousserver/SocketHandler.kt
@@ -0,0 +1,17 @@
+package com.github.p2pfilestream.rendezvousserver
+
+import org.springframework.stereotype.Component
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketSession
+import org.springframework.web.socket.handler.TextWebSocketHandler
+
+@Component
+class SocketHandler : TextWebSocketHandler() {
+    override fun afterConnectionEstablished(session: WebSocketSession) {
+        println("afterConnectionEstablished")
+    }
+
+    override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
+        println(message)
+    }
+}
---
+++ rendezvous-server/src/main/kotlin/com/github/p2pfilestream/rendezvousserver/WebSocketConfig.kt
@@ -0,0 +1,15 @@
+package com.github.p2pfilestream.rendezvousserver
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.socket.config.annotation.EnableWebSocket
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+
+@Configuration
+@EnableWebSocket
+class WebSocketConfig(val socketHandler: SocketHandler) : WebSocketConfigurer {
+    override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
+        registry.addHandler(socketHandler, "/ws/*")
+            .setAllowedOrigins("*")
+    }
+}
