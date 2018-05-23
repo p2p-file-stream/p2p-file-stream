a6b6e6232b80b849347c40b5e0fc8686c51f9d7b
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-23 12:07:11 +0200

    Remove prePostEnabled = true

--- account-server/src/main/kotlin/com/github/p2pfilestream/accountserver/ApplicationSecurity.kt
+++ account-server/src/main/kotlin/com/github/p2pfilestream/accountserver/ApplicationSecurity.kt
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -24,7 +23,6 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore
  */
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     @Value("\${security.signing-key}")
