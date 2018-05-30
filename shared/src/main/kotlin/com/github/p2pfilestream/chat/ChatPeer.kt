510eb1606ecf545363e4b8cd023578ffdbb6d4da
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-05-30 10:38:35 +0200

    Add text() to ChatPeer

--- shared/src/main/kotlin/com/github/p2pfilestream/chat/ChatPeer.kt
+++ shared/src/main/kotlin/com/github/p2pfilestream/chat/ChatPeer.kt
@@ -3,4 +3,5 @@ package com.github.p2pfilestream.chat
 interface ChatPeer {
     fun binary(binaryMessage: BinaryMessage)
     fun chunk(binaryMessageChunk: BinaryMessageChunk)
+    fun text(textMessage: TextMessage)
 }
