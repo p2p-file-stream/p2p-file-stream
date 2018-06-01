3f46b7705c0810797a83c29ab3c537ff0fbaf6c7
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-06-01 09:36:13 +0200

    Delegate encoding JSON to MessageEncoder

---
+++ shared/src/main/kotlin/com/github/p2pfilestream/rendezvous/MessageEncoder.kt
@@ -0,0 +1,13 @@
+package com.github.p2pfilestream.rendezvous
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import kotlin.reflect.KFunction
+
+abstract class MessageEncoder(private val receiver: (ByteArray) -> Unit) {
+    private val mapper = jacksonObjectMapper()
+
+    protected fun message(type: KFunction<*>, vararg elements: Any) {
+        val message = WebSocketMessage(type.name, elements.asList())
+        receiver(mapper.writeValueAsBytes(message))
+    }
+}
