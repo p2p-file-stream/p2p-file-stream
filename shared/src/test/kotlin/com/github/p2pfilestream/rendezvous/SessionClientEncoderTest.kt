eaf87c37cf13ba9ceb919c5542b3972f22f99d2a
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-06-01 19:17:57 +0200

    Test encoding of all methods

--- shared/src/test/kotlin/com/github/p2pfilestream/rendezvous/SessionClientEncoderTest.kt
+++ shared/src/test/kotlin/com/github/p2pfilestream/rendezvous/SessionClientEncoderTest.kt
@@ -1,36 +1,34 @@
 package com.github.p2pfilestream.rendezvous
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.p2pfilestream.Account
 import com.github.p2pfilestream.Device
 import com.github.p2pfilestream.encoding.MessageDecoder
 import io.mockk.mockk
 import io.mockk.verify
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class SessionClientEncoderTest {
-    private val encoder = SessionClientEncoder { messages.add(it) }
-    private val messages = ArrayList<ByteArray>()
-    private val mapper = jacksonObjectMapper()
+    private val encoder = SessionClientEncoder { decoder.decode(it) }
+    private val mock = mockk<SessionClient>(relaxed = true)
+    private val decoder = MessageDecoder(mock)
 
     @Test
     fun deleteRequest() {
-        val device = Device("Henk", Account("Piet", 3), 4)
-        encoder.request(device)
-        assertThat(messages).hasSize(1)
-        println(String(messages[0]))
-        val client = mockk<SessionClient>(relaxed = true)
-        val decoder = MessageDecoder(client)
-        decoder.decode(messages[0])
-        verify { client.request(device) }
+        encoder.deleteRequest("Henk")
+        verify { mock.deleteRequest("Henk") }
     }
 
     @Test
     fun request() {
+        val device = Device("Henk", Account("Piet", 3), 4)
+        encoder.request(device)
+        verify { mock.request(device) }
     }
 
     @Test
     fun response() {
+        val device = Device("Henk", Account("Piet", 3), 4)
+        encoder.request(device)
+        verify { mock.request(device) }
     }
 }
