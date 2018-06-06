f8f56c47ae23327b04cac84e7708dbfbd5dabf7f
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-06-06 08:50:45 +0200

    Add comments to MessageDecoder

--- shared/src/main/kotlin/com/github/p2pfilestream/encoding/MessageDecoder.kt
+++ shared/src/main/kotlin/com/github/p2pfilestream/encoding/MessageDecoder.kt
@@ -5,6 +5,9 @@ import kotlin.reflect.full.declaredFunctions
 import kotlin.reflect.full.valueParameters
 import kotlin.reflect.jvm.javaType
 
+/**
+ * Decodes json, and calls the method on an interface.
+ */
 class MessageDecoder<T : Any>(
     private val receiver: T
 ) {
@@ -16,12 +19,15 @@ class MessageDecoder<T : Any>(
         val message = mapper.readTree(bytes)
         val type = message[WebSocketMessage::type.name].asText()
         val jsonArguments = message[WebSocketMessage::arguments.name];
+        // Find the method with name specified in type
         val function = types[type] ?: throw IllegalArgumentException("Type not found")
+        // Parse the arguments to the right types
         val arguments = arrayOfNulls<Any?>(function.valueParameters.size)
         for ((i, parameter) in function.valueParameters.withIndex()) {
             val javaType = mapper.typeFactory.constructType(parameter.type.javaType)
             arguments[i] = mapper.readValue(mapper.treeAsTokens(jsonArguments[i]), javaType)
         }
+        // Call the method
         function.call(receiver, *arguments)
     }
 }
