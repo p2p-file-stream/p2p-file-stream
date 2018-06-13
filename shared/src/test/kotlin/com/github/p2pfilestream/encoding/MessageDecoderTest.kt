b5eb1390e29e3f448dcda25e1a4fb2e210abf154
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-06-13 08:55:38 +0200

    Test MessageEncoder and Decoder with generic arguments

--- shared/src/test/kotlin/com/github/p2pfilestream/encoding/MessageDecoderTest.kt
+++ shared/src/test/kotlin/com/github/p2pfilestream/encoding/MessageDecoderTest.kt
@@ -1,26 +1,44 @@
 package com.github.p2pfilestream.encoding
 
+import com.github.p2pfilestream.Account
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Test
 
 class MessageDecoderTest {
     @Test
-    fun `Decode to private class`() {
-        val mock = mockk<MyInterface>(relaxed = true)
-        val decode = MessageDecoder(mock)
-        val encoder = MessageEncoder.create<MyInterface> { decode(it) }
+    fun `Encode and decode some method call`() {
+        val (mock, encoder) = getMock()
         encoder.doStuff()
         verify {
             mock.doStuff()
         }
     }
 
-    private interface MyInterface {
-        fun doStuff()
-    }
-
-    private abstract class Private : MyInterface {
-        abstract override fun doStuff()
+    @Test
+    fun `Call method with generic argument`() {
+        val (mock, encoder) = getMock()
+        val accounts = listOf(Account("Hello", "World"))
+        encoder.genericStuff(accounts)
+        verify {
+            mock.genericStuff(accounts)
+        }
     }
 }
+
+private interface MyInterface {
+    fun doStuff()
+    fun genericStuff(accounts: List<Account>)
+}
+
+private fun getMock(): MyMock {
+    val mock: MyInterface = mockk(relaxed = true)
+    val decode: MessageDecoder<MyInterface> = MessageDecoder(mock)
+    val encoder: MyInterface = MessageEncoder.create { decode(it) }
+    return MyMock(mock, encoder)
+}
+
+private data class MyMock(
+    val mock: MyInterface,
+    val encoder: MyInterface
+)
